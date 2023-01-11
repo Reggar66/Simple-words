5abf35b0e8e71a3145a2d8ea7be1ef9a7b57e597
Reggar66 <grzesiekbak96@gmail.com>
2023-01-11 20:42:53 +0100

    database offline mode on

--- app/src/main/java/com/ada/simplewords/SimpleWordsApplication.kt
+++ app/src/main/java/com/ada/simplewords/SimpleWordsApplication.kt
@@ -2,6 +2,8 @@ package com.ada.simplewords
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,5 +11,6 @@ class SimpleWordsApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        Firebase.database.setPersistenceEnabled(true)
     }
 }
