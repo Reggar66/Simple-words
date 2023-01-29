18af89e8c0dafefc7507bc3f90ed7228a87814ff
Reggar66 <grzesiekbak96@gmail.com>
2023-01-29 11:38:03 +0100

    clean up

--- feature/quizdetails/src/main/java/com/ada/quizdetails/QuizDetailsScreen.kt
+++ feature/quizdetails/src/main/java/com/ada/quizdetails/QuizDetailsScreen.kt
@@ -1,20 +1,16 @@
 package com.ada.quizdetails
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ada.common.OnClick
